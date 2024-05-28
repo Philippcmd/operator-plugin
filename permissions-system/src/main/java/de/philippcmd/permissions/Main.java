d7a2da610d20637ccffb072263b65de7fe8430c3
Philippcmd <139921614+Philippcmd@users.noreply.github.com>
2024-05-28 21:13:43 +0200

    Update

--- permissions-system/src/main/java/de/philippcmd/permissions/Main.java
+++ permissions-system/src/main/java/de/philippcmd/permissions/Main.java
@@ -1,1 +1,27 @@
+package de.philippcmd.permissions;
+
+import de.philippcmd.permissions.commands.PermissionCommand;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public final class Main extends JavaPlugin {
+
+    private HashMap<UUID, PermissionAttachment> playerPermissions = new HashMap<>();
+    public HashMap<UUID, PermissionAttachment> getPlayerPermissions() {
+        return playerPermissions;
+    }
+
+    @Override
+    public void onEnable() {
+        getCommand("permission").setExecutor(new PermissionCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
 
