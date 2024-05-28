b6287bd89fd475dd639e84646cb89b02002e336d
Philippcmd <139921614+Philippcmd@users.noreply.github.com>
2024-05-28 21:51:29 +0200

    Update

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+}
+
+group = "de.philippCMD"
+version = "1.20.1_1.0"
+
+allprojects {
+    apply {
+        plugin<JavaPlugin>()
+    }
+
+    repositories {
+        mavenCentral()
+        maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+        maven("https://oss.sonatype.org/content/groups/public/")
+    }
+
+    dependencies {
+        compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
+    }
+
+    java{
+        toolchain{
+            languageVersion = JavaLanguageVersion.of(17)
+        }
+    }
+}
