352e35b5a72bad6eef0f9cae81638ab2a746f219
Philippcmd <139921614+Philippcmd@users.noreply.github.com>
2024-05-30 11:15:08 +0200

    Update

---
+++ permissions-system/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+}
+
+bukkit {
+    name = "PermissionsSystem"
+    main = "de.philippcmd.permissions-system.Main"
+    apiVersion = "1.20"
+    version = rootProject.version as String
+
+    commands {
+        register("permission") {
+            description = "manages permissions of players"
+            usage = "/permission <give|remove> <permission> <player>"
+            aliases = listOf("perm")
+            permission = "permissions.manage"
+            permissionMessage = "You have no permission to run this command"
+        }
+    }
+}
